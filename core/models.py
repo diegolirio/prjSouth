76d16fcf0f65a18115cb28e2b94d650e2fc2750f
Diego Lirio <diegolirio.dl@gmail.com>
2014-01-16 23:28:51 -0200

    ....

---
+++ core/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Endereco(models.Model):
+	logradouro = models.CharField(max_length=100)
+	numero = models.IntegerField(default=0)
+
+class Pessoa(models.Model):
+	nome = models.CharField(max_length=50)
+	cpf_cnpj = models.CharField(max_length=20)
+	endereco = models.ForeignKey(Endereco)
+	
+
